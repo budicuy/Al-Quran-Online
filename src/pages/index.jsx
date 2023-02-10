281a5113fa3a65222ac5cee4305ca28c962cbabf
budicuy <personal.budicuy@gmail.com>
2023-02-10 19:58:24 +0800

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+    const [surah, setSurah] = useState([]);
+    const [search, setSearch] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const getSurah = async () => {
+        setLoading(true);
+        try {
+            let response = await axios.get("https://equran.id/api/v2/surat");
+            setSurah(response.data.data);
+            setLoading(false);
+        } catch (e) {
+            console.log(e.message);
+        }
+    };
+
+    useEffect(() => {
+        getSurah();
+    }, []);
+
+    return (
+        <div>
+            <Head>
+                <title>Al-Qur'an</title>
+            </Head>
+            <Navbar />
+            <div className="container px-5 md:px-14">
+                <form>
+                    <input
+                        type="text"
+                        className="w-full px-4 py-2 mt-5 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-purple-600"
+                        placeholder="Cari Surah"
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </form>
+                <div className="w-full mt-5">
+                    <div className="grid gap-3 text-purple-500 md:grid-cols-3">
+                        {loading ? (
+                            <div className="flex justify-center">
+                                <svg
+                                    className="w-10 h-10 text-purple-500 animate-spin"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        stroke-width="4"
+                                    ></circle>
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8v8z"
+                                    ></path>
+                                </svg>
+                            </div>
+                        ) : (
+                            surah
+                                .filter((surah) => {
+                                    // ketika search kosong maka akan menampilkan semua surah yang ada di API
+                                    // jika tidak maka akan menampilkan namalatin surah yang sesuai dengan inputan user
+                                    if (search == "") {
+                                        return surah;
+                                    }
+                                    if (
+                                        surah.namaLatin
+                                            // abaikan huruf besar kecil spasi dan simbol yang ada di inputan user
+                                            .toLowerCase()
+                                            .replace(/[^a-zA-Z ]/g, "")
+                                            .includes(
+                                                search
+                                                    .toLowerCase()
+                                                    .replace(/[^a-zA-Z ]/g, "")
+                                            )
+                                    ) {
+                                        return surah;
+                                    }
+                                })
+                                .map((surah) => (
+                                    <div className="p-4 bg-gray-100 rounded shadow-md cursor-pointer hover:bg-white">
+                                        <h3 className="text-lg font-semibold ">
+                                            No. {surah.nomor} {surah.namaLatin}
+                                        </h3>
+                                        <hr className="border-b-1" />
+                                        <p className="mt-5 text-5xl text-right ">
+                                            {surah.nama}
+                                        </p>
+                                        <p>{surah.arti}</p>
+                                    </div>
+                                ))
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            poppins: ['Poppins', 'sans-serif'],
+            quicksand: ['Quicksand', 'sans-serif'],
+        },
+        container: {
+            center: true,
+            maxWidth: "1536",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
