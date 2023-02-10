b80d50963c677f83b440335925a896c5a2ceb76d
budicuy <personal.budicuy@gmail.com>
2023-02-11 06:20:43 +0800

    optimaze

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,8 @@
-// eslint-disable-next-line import/no-unresolved
 import '@/styles/globals.css'
 
-// eslint-disable-next-line react/prop-types
 export default function App({
-  Component,
-  pageProps,
+    Component,
+    pageProps,
 }) {
-  // eslint-disable-next-line react/react-in-jsx-scope, react/jsx-filename-extension
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
