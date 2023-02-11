4ef5375469de7dd55bac7038cf757640a2cfd22c
budicuy <personal.budicuy@gmail.com>
2023-02-11 18:50:43 +0800

    new loading bar router

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import nProgress from 'nprogress'
+import Router from 'next/router'
+import "nprogress/nprogress.css"
 
+Router.events.on('routeChangeStart', () => nProgress.start())
+Router.events.on('routeChangeComplete', () => nProgress.done())
+Router.events.on('routeChangeError', () => nProgress.done())
+
+// off spinner
+nProgress.configure({ showSpinner: false })
+// height bar nprogress 1rem
+nProgress.configure({ minimum: 1 })
 export default function App({
     Component,
     pageProps,
