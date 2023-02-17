575bb88156e276e1626f19a70ebdaba483be1454
budicuy <personal.budicuy@gmail.com>
2023-02-18 04:09:11 +0800

    fix PWA bug

--- src/meta/SeoMeta.jsx
+++ src/meta/SeoMeta.jsx
@@ -3,6 +3,7 @@ export default function SeoMeta() {
         <>
             {/* Seo Meta Tiitle & Desc */}
             <meta name="title" content="MyQuran" />
+            <link rel="manifest" href="/manifest.json" />
             <meta
                 name="description"
                 content="MyQuran adalah aplikasi web yang berisi Al-Quran lengkap dengan terjemahan Bahasa Indonesia dan audio."
